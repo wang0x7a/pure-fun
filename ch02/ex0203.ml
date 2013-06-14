8961d7ac933d7b2b46bcab319380a3c7c5270ff9
Zhe Wang <zhexwang@gmail.com>
2013-06-14 19:19:54 +0800

    solution to Exercise 2.3

---
+++ ch02/ex0203.ml
@@ -0,0 +1,24 @@
+(* Exercise 2.3
+ * Inserting an existing element into a binary search tree copies the entire
+ * search path even though the copied nodes are indistinguishable from the 
+ * originals. Rewrite insert using exceptions to avoid this copying. Establish
+ * only one handler per insertion rather than one handler per iteration.
+ *)
+
+type 'a bst = Nil | Tree of 'a bst * 'a * 'a bst
+
+exception ExistingElement
+
+let rec insert (x : 'a) (tree : 'a bst) : 'a bst =
+  match tree with
+  | Nil -> Tree (Nil, x, Nil)
+  | Tree (l, y, r) ->
+    (* One of the keys to understanding functional data structures is
+     * the copy-share process. For example,
+     * A copy is made by constructing a new Tree (Tree (l, y, insert x r)),
+     * and l and y, in this example, are shared by the copy.
+     *)
+    if x > y then Tree (l, y, insert x r)
+    else if x < y then Tree (insert x l, y, r)
+    else raise ExistingElement
+;;
